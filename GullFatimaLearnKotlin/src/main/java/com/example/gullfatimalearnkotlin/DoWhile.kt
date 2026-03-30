1e609fc874d7807926941c669a97e39e7c20d844
GULL FATIMA <gullfatimah14@gmail.com>
2026-03-31 00:11:02 +0500

    Topic While,doWhile,for,Ranges done

---
+++ GullFatimaLearnKotlin/src/main/java/com/example/gullfatimalearnkotlin/DoWhile.kt
@@ -0,0 +1,19 @@
+package com.example.gullfatimalearnkotlin
+
+fun main()
+{
+    var number=1
+    do {
+        println("greater")
+        number++
+    }while(number>10)
+
+    var i=3
+    do {
+        println("1. Register")
+        println("2. Login")
+        println("3. Exit")
+        i++
+    }while(i<=2)
+}
+
---
+++ GullFatimaLearnKotlin/src/main/java/com/example/gullfatimalearnkotlin/While.kt
@@ -0,0 +1,23 @@
+package com.example.gullfatimalearnkotlin
+
+fun main() {
+    var a=1
+
+    //while loop
+    while (a<=10)
+    {
+        println(a)
+        a++
+    }
+
+    //printing table
+    var i=4
+    var j=1
+    while(j<=10)
+    {
+        println("$i * $j= ${(i*j)}")
+        j++
+        i+4
+    }
+
+}

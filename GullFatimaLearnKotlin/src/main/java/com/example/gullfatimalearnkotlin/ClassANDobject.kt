9419a87de9c41a0407762200da67781e0c11e009
GULL FATIMA <gullfatimah14@gmail.com>
2026-03-31 00:12:38 +0500

    Topic class,object,constructor,getter,seter done

---
+++ GullFatimaLearnKotlin/src/main/java/com/example/gullfatimalearnkotlin/ClassANDobject.kt
@@ -0,0 +1,14 @@
+package com.example.gullfatimalearnkotlin
+
+fun main()
+{
+    val p1=person()
+    p1.name="Asad"
+    p1.age=0
+}
+
+class person
+{
+    var name=""
+    var age=0
+}
---
+++ GullFatimaLearnKotlin/src/main/java/com/example/gullfatimalearnkotlin/Constructor.kt
@@ -0,0 +1,54 @@
+package com.example.gullfatimalearnkotlin
+
+fun main()
+{
+    var c1= calculator()
+    c1.add(23,4)
+    var std1=Students("Asad","24011519-010",19,24,4)
+    var std2=Students("Riya","24011519-011",21,23,6)
+    std1.display()
+    println(std2.name)
+    var p1=PersonL("Gull")
+    var p2=PersonL("amal",17)
+}
+
+//default constructor will be called.
+//default constructor has zero parameter
+class calculator
+{
+    fun add(a:Int, b:Int)
+    {
+        println(a+b)
+    }
+
+}
+//primary constructor
+class Students(var name:String,val RollNo:String, var Age:Int, val batch:Int, var semester:Int )
+{
+    fun display(){
+        println("Name: $name")
+        println("RollNo: $RollNo")
+        println("Batch: $batch")
+        println("Name: $semester")
+    }
+}
+
+open class PersonL
+{
+    var name:String=""
+    var age:Int=0
+
+    //Secondary constructor
+    constructor(name:String)
+    {
+        this.name=name
+        this.age=19
+    }
+    constructor(name:String,age:Int)
+    {
+        this.name=name
+        this.age=age
+    }
+
+}
+

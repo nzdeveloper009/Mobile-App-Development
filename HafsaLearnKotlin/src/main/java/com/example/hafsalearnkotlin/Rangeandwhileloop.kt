8c16cd5381f11c6752e4c17152cb3052178a529f
Hafsajaved09 <hafsaj909@gmail.com>
2026-04-03 12:16:12 -0700

    whilestatment

---
+++ HafsaLearnKotlin/src/main/java/com/example/hafsalearnkotlin/Rangeandwhileloop.kt
@@ -0,0 +1,11 @@
+package com.example.hafsalearnkotlin
+fun main(){
+    var num=2
+    var index=1
+    println("Table of two:")
+    while(index<=10){
+        var result=num*index
+        println("$num * $index= "+ result)
+            index++
+    }
+}
